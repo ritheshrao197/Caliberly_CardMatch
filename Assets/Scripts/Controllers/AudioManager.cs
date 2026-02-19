a0de0248c5142d2c56da648292ed159b19873749
Rithesh U <rithesh197rao@email.com>
2026-02-19 09:54:02 +0530

    Add AudioManager class for managing audio clips and settings

---
+++ Assets/Scripts/Controllers/AudioManager.cs
@@ -0,0 +1,181 @@
+using UnityEngine;
+using MemoryGame.Constants;
+using MemoryGame.Events;
+using MemoryGame.UI.Events;
+
+namespace MemoryGame.Controller
+{
+    public class AudioManager : MonoBehaviour
+    {
+        // ---------- Clips ----------
+        [Header("Card SFX")]
+        [SerializeField] private AudioClip flip;
+        [SerializeField] private AudioClip match;
+        [SerializeField] private AudioClip mismatch;
+
+        [Header("UI SFX")]
+        [SerializeField] private AudioClip click;
+        [SerializeField] private AudioClip popupOpen;
+        [SerializeField] private AudioClip popupClose;
+
+        [Header("Level SFX")]
+        [SerializeField] private AudioClip levelStart;
+        [SerializeField] private AudioClip levelWin;
+        [SerializeField] private AudioClip levelFail;
+
+        [Header("Music")]
+        [SerializeField] private AudioClip bgMusic;
+        [SerializeField] private bool playMusicOnAwake = true;
+
+        [Header("Volumes")]
+        [Range(0f, 1f)] [SerializeField] private float sfxVolume = AudioConstants.DefaultSfxVolume;
+        [Range(0f, 1f)] [SerializeField] private float musicVolume = AudioConstants.DefaultMusicVolume;
+
+        private AudioSource _sfx;
+        private AudioSource _music;
+        private EventBus _bus;
+
+        private bool _sfxEnabled;
+        private bool _musicEnabled;
+
+        private const string KEY_SFX = AudioConstants.SfxEnabledKey;
+        private const string KEY_MUSIC = AudioConstants.MusicEnabledKey;
+
+        #region Unity Lifecycle
+
+        private void Awake()
+        {
+            _bus = EventBus.Instance; // cache once
+
+            InitializeAudioSources();
+            LoadPreferences();
+            InitializeMusic();
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        #endregion
+
+        #region Initialization
+
+        private void InitializeAudioSources()
+        {
+            _sfx = gameObject.AddComponent<AudioSource>();
+            _sfx.playOnAwake = false;
+
+            _music = gameObject.AddComponent<AudioSource>();
+            _music.loop = true;
+            _music.playOnAwake = false;
+            _music.clip = bgMusic;
+            _music.volume = musicVolume;
+        }
+
+        private void LoadPreferences()
+        {
+            _sfxEnabled = PlayerPrefs.GetInt(KEY_SFX, 1) == 1;
+            _musicEnabled = PlayerPrefs.GetInt(KEY_MUSIC, 1) == 1;
+        }
+
+        private void InitializeMusic()
+        {
+            if (playMusicOnAwake && _musicEnabled && _music.clip != null)
+                _music.Play();
+        }
+
+        #endregion
+
+        #region Event Subscription
+
+        private void SubscribeEvents()
+        {
+            // Gameplay
+            _bus.Subscribe<CardSelectedEvent>(_ => PlaySfx(flip));
+            _bus.Subscribe<PairMatchedEvent>(_ => PlaySfx(match));
+            _bus.Subscribe<PairMismatchedEvent>(_ => PlaySfx(mismatch));
+            _bus.Subscribe<GameWonEvent>(_ => PlaySfx(levelWin));
+            _bus.Subscribe<GameLostEvent>(_ => PlaySfx(levelFail));
+            _bus.Subscribe<LevelStartedEvent>(OnLevelStarted);
+
+            // UI
+            _bus.Subscribe<PauseEvent>(_ => PlaySfx(popupOpen));
+            _bus.Subscribe<ShowLevelSelectEvent>(_ => PlaySfx(popupOpen));
+
+            _bus.Subscribe<ResumeEvent>(_ => PlaySfx(popupClose));
+            _bus.Subscribe<HideLevelSelectEvent>(_ => PlaySfx(popupClose));
+
+            _bus.Subscribe<GoHomeEvent>(_ => PlaySfx(click));
+            _bus.Subscribe<RestartEvent>(_ => PlaySfx(click));
+            _bus.Subscribe<StartFromHomeEvent>(_ => PlaySfx(click));
+            _bus.Subscribe<StartLevelEvent>(_ => PlaySfx(click));
+
+            // Settings
+            _bus.Subscribe<ToggleSfxEvent>(_ => ToggleSfx());
+            _bus.Subscribe<ToggleMusicEvent>(_ => ToggleMusic());
+        }
+
+        private void UnsubscribeEvents()
+        {
+            _bus.Clear(); 
+            // Since this AudioManager subscribes anonymously,
+            // safest option is clearing if it owns global audio.
+            // If not, switch to stored delegates instead.
+        }
+
+        #endregion
+
+        #region Gameplay Handlers
+
+        private void OnLevelStarted(LevelStartedEvent e)
+        {
+            PlaySfx(levelStart);
+
+            if (_musicEnabled && _music.clip != null && !_music.isPlaying)
+                _music.Play();
+        }
+
+        #endregion
+
+        #region Core Audio Logic
+
+        private void PlaySfx(AudioClip clip)
+        {
+            if (!_sfxEnabled || clip == null)
+                return;
+
+            _sfx.PlayOneShot(clip, sfxVolume);
+        }
+
+        private void ToggleSfx()
+        {
+            _sfxEnabled = !_sfxEnabled;
+            PlayerPrefs.SetInt(KEY_SFX, _sfxEnabled ? 1 : 0);
+        }
+
+        private void ToggleMusic()
+        {
+            _musicEnabled = !_musicEnabled;
+            PlayerPrefs.SetInt(KEY_MUSIC, _musicEnabled ? 1 : 0);
+
+            if (_musicEnabled)
+            {
+                if (_music.clip != null && !_music.isPlaying)
+                    _music.Play();
+            }
+            else
+            {
+                if (_music.isPlaying)
+                    _music.Stop();
+            }
+        }
+
+        #endregion
+    }
+}
